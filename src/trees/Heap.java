bfec4e3c4f4c7740471fcef68e53f683fcd9b30c
Erik S. Carlsten <ecarlste.register@gmail.com>
2012-11-04 21:57:20 -0700

    Created new branch "heap" from "master". Created new trees package and a Heap class.

---
+++ src/trees/Heap.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trees;
+
+/**
+ *
+ * @author es.carlsten
+ */
+public class Heap {
+    
+}
