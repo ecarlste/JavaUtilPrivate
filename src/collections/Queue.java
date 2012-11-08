40055342c60235b2fd20a39d6f93a9eec479bea3
Erik S. Carlsten <ecarlste@gmail.com>
2012-11-08 14:42:36 -0700

    Added Queue and QueueNode classes to the collections package.

---
+++ src/collections/Queue.java
@@ -0,0 +1,58 @@
+
+package collections;
+
+import java.util.NoSuchElementException;
+
+public class Queue<T> {
+    
+    QueueNode<T> head;
+    QueueNode<T> tail;
+    
+    public boolean insert(T object)
+    {
+        QueueNode<T> newNode = new QueueNode<>(object);
+        
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            tail.setNext(newNode);
+        }
+        
+        tail = newNode;
+        
+        return true;
+    }
+    
+    public T peek()
+    {
+        return head.getValue();
+    }
+    
+    public T remove()
+    {
+        if (head != null)
+        {
+            T value = head.getValue();
+            head = head.getNext();
+            
+            if (head == null)
+            {
+                
+            }
+            
+            return value;
+        }
+        else
+        {
+            throw new NoSuchElementException();
+        }
+    }
+    
+    public boolean isEmpty()
+    {
+        return (head == null && tail == null);
+    }
+}
